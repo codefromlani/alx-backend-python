cad49719261ddf45a1794b28752f074f38cd20f7
codefromlani <rodiathammed48@gmail.com>
2025-09-30 23:25:16 +0100

    feat: track message edits and store old content in MessageHistory

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -10,3 +10,23 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if not instance.pk:
+        # Message is new, no history to save
+        return
+    
+    try:
+        old_message = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        return
+    
+    if old_message.content != instance.content:
+        # Save old content to history
+        MessageHistory.objects.create(
+            message=old_message,
+            old_content=old_message.content
+        )
+        # Mark message as edited
+        instance.edited = True
