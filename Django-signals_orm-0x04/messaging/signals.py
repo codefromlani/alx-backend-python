56708d8e0011881cb0f493601a03a86b041fb02d
codefromlani <rodiathammed48@gmail.com>
2025-10-02 00:06:24 +0100

    feat: cascade delete user-related messages, notifications, and histories via signals

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 
@@ -31,3 +32,11 @@ def log_message_edit(sender, instance, **kwargs):
         )
         # Mark message as edited
         instance.edited = True
+
+@receiver(post_delete, sender=User)
+def delete_user_related(sender, instance, **kwargs):
+    MessageHistory.objects.filter(edited_by=instance).delete()
+
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
-# Create your views here.
+@login_required
+@require_POST
+def delete_user(request):
+    user = request.user
+    logout(request)         # log out first so session is gone
+    user.delete()           # triggers pre_delete/post_delete signals
+    return redirect('home') 
