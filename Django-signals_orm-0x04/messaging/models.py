77208b35974a53a2479daeab4b7221efbd8a29dc
codefromlani <rodiathammed48@gmail.com>
2025-10-03 23:31:12 +0100

    refactor: move UnreadMessagesManager to messaging/managers.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .managers import UnreadMessagesManager
 
-
-class UnreadMessagesManager(models.Manager):
-    def for_user(self, user):
-        return (
-            self.get_queryset()
-            .filter(receiver=user, read=False)
-            .only("id", "sender", "content", "timestamp")
-        )
-    
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
@@ -26,7 +18,7 @@ class Message(models.Model):
         related_name="replies"
     )
 
-    objects = models.Manager()  
+    objects = models.Manager()
     unread = UnreadMessagesManager()
 
     def __str__(self):
