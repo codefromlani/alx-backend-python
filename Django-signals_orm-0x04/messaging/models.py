4c1dc9e49a91026149c12707dc2d282ed528f678
codefromlani <rodiathammed48@gmail.com>
2025-10-03 23:11:47 +0100

    feat: add read field and custom manager for unread messages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return (
+            self.get_queryset()
+            .filter(receiver=user, read=False)
+            .only("id", "sender", "content", "timestamp")
+        )
+    
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
     parent_message = models.ForeignKey(
         "self",
         null=True,
@@ -15,6 +25,9 @@ class Message(models.Model):
         on_delete=models.CASCADE,
         related_name="replies"
     )
+
+    objects = models.Manager()  
+    unread = UnreadMessagesManager()
 
     def __str__(self):
         if self.parent_message:
